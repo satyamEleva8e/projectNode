7d8ea9ce396dac6743a9d1b58977030cb3e00808
adeshsuryandev <adeshsuryan2013@gmail.com>
2024-06-24 10:44:59 +0530

    start express

---
+++ testing.js
@@ -0,0 +1,10 @@
+// GET(read), POST(Create), PUT(Update), delete(Delete)
+
+const express = require("express");
+const app = express();
+
+const PORT = 8000;
+
+// app.API_Method(route, callback)
+
+app.get("/", () => {});
